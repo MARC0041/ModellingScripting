63fe99d1388a12ab8752ef2202c36d2362f1feef
@marcus.chan <marcus.chan@hyperganic.com>
2023-07-04 11:23:16 +0800

    modelling in python working on local

--- ModellingInPython/helper_functions.py
+++ ModellingInPython/helper_functions.py
@@ -1,7 +1,8 @@
 import math
 # from ModellingInPython import matplotlib.pyplot as plt
+# from ModellingInPython import numpy as np
 import numpy as np
-import ipyvolume as ipv
+# import ipyvolume as ipv
 
 # /System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python: No module named install
 # export PATH="/Users/marcusc/opt/anaconda3/envs/python311/bin:$PATH"
@@ -9,9 +10,11 @@ import ipyvolume as ipv
 def create_circle_vectors(radius = 5, num_points = 10):
     vectors = []
     for i in range(num_points):
+        # pass
         theta = 2 * math.pi * float(i) / num_points
         x, y = radius * math.cos(theta), radius * math.sin(theta)
-        vectors.append(x,y,0)
+        vectors.append((x,y,0))
+    return vectors
         
 
 
@@ -21,10 +24,10 @@ def create_circle_vectors(radius = 5, num_points = 10):
 # Create some 3D vectors
 def visualise_vectors(vectors = [[0,0,0]]):
     N = 1000
-    x,y,z = np.random.normal(0,1,(3,N))
-    fig = ipv.figure()
-    scatter = ipv.scatter(x,y,z, marker = 'sphere')
-    ipv.show()
+    # x,y,z = np.random.normal(0,1,(3,N))
+    # fig = ipv.figure()
+    # scatter = ipv.scatter(x,y,z, marker = 'sphere')
+    # ipv.show()
 def test_fn():
     print("hello world")
     return 2.5
