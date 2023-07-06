3539753522e23dad84d793353cbcd00c8ea716c5
@marcus.chan <marcus.chan@hyperganic.com>
2023-07-06 15:46:02 +0800

    sweeping functionalities

--- ModellingInPython/helper_functions.py
+++ ModellingInPython/helper_functions.py
@@ -7,9 +7,10 @@ from geomdl import BSpline, utilities
 # /System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python: No module named install
 # export PATH="/Users/marcusc/opt/anaconda3/envs/python311/bin:$PATH"
 
-def create_circle_vectors(radius = 1, num_points = 50, z_height = 0):
+def get_circle_vectors(radius = 1, num_points = 50, z_height = 0):
     """
     Returns a list of vectors that form a circle
+
     Inputs:
         radius: float
         num_points: int
@@ -20,10 +21,11 @@ def create_circle_vectors(radius = 1, num_points = 50, z_height = 0):
         x, y = radius * math.cos(theta), radius * math.sin(theta)
         z = float(z_height)
         vectors.append((x,y,z))
-    return vectors
+    return np.array(vectors)
 def visualise_vectors(vectors = [[0.0,0.0,0.0]], point_size = 1):
     """Visualises the vectors inputted in a notebook
     Takes in a list of vectors and a point_size for each vector
+
     Inputs: 
         vectors: List<List<float, float, float>> 
         point_size: float
@@ -33,6 +35,10 @@ def visualise_vectors(vectors = [[0.0,0.0,0.0]], point_size = 1):
     y = [v[1] for v in vectors]
     z = [v[2] for v in vectors]
     scatter = ipv.scatter(x,y,z, marker = 'sphere',size=point_size)
+    ipv.xlim(min(x), max(x))
+    ipv.ylim(min(y), max(y))
+    ipv.zlim(min(z), max(z))
+    ipv.squarelim()
     ipv.show()
 def unit_vector(vector):
     """ Returns the unit vector of the vector."""
@@ -54,14 +60,16 @@ def z_rotation(vector,theta):
     """Rotates 3-D vector around z-axis"""
     R = np.array([[np.cos(theta), -np.sin(theta),0],[np.sin(theta), np.cos(theta),0],[0,0,1]])
     return np.dot(R,vector)
-def rotate_vector(vector, axis, angle):
+def rotate_vector(vector, axis, angle, deg = True):
     """Rotate a vector around an axis by some angle
+
     Inputs:
         vector: List<float, float, float>
         axis: <float, float, float>
-        angle: float (in degrees)"""
+        angle: float (in degrees)
+        deg: bool (if the angle is in degree; if the angle is in radians, use False)"""
     # Convert angle to radians
-    angle_rad = np.radians(angle)
+    angle_rad = np.radians(angle) if deg else angle
     # rot_vector = angle_rad * np.array(axis)
     # rotation = R.from_rotvec(rot_vector)
     # rotated_vector = rotation.apply(vector)
@@ -79,9 +87,37 @@ def rotate_vector(vector, axis, angle):
     # Apply the rotation to the vector
     rotated_vector = np.dot(rotation_matrix, vector)
     return rotated_vector
-def get_spline_vectors_from_controlpts(controlpts, sample_count=10):
+def rotate_vectors(vectors, axis, angle, deg = True):
+    """Rotate a list of vectors around an axis by some angle. 
+    All vectors in the list will be rotated by the same rotation matrix.
+
+    Inputs:
+        vector: List<float, float, float>
+        axis: <float, float, float>
+        angle: float (in degrees)"""
+    # Convert angle to radians
+    angle_rad = np.radians(angle) if deg else angle
+    # rot_vector = angle_rad * np.array(axis)
+    # rotation = R.from_rotvec(rot_vector)
+    # rotated_vector = rotation.apply(vector)
+
+    # Normalize the axis vector
+    axis = axis / np.linalg.norm(axis)
+    
+    # Compute the rotation matrix
+    c = np.cos(angle_rad)
+    s = np.sin(angle_rad)
+    rotation_matrix = np.array([[c + axis[0]**2*(1-c), axis[0]*axis[1]*(1-c) - axis[2]*s, axis[0]*axis[2]*(1-c) + axis[1]*s],
+                                [axis[1]*axis[0]*(1-c) + axis[2]*s, c + axis[1]**2*(1-c), axis[1]*axis[2]*(1-c) - axis[0]*s],
+                                [axis[2]*axis[0]*(1-c) - axis[1]*s, axis[2]*axis[1]*(1-c) + axis[0]*s, c + axis[2]**2*(1-c)]])
+    
+    # Apply the rotation to the vector
+    rotated_vector = np.dot(rotation_matrix, vectors.T).T
+    return rotated_vector
+def get_spline_vectors_from_controlpts(controlpts, sample_count=10, degree = 3):
     """
     Returns a list of points that follows the derived curve using bspline
+
     Inputs: 
         controlpts: list of control points in a list. e.g. [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
         sample_count: length of the returned vector list
@@ -90,7 +126,7 @@ def get_spline_vectors_from_controlpts(controlpts, sample_count=10):
     """
     crv = BSpline.Curve()
     # Set curve degree
-    crv.degree = max(1, min(3, len(controlpts)-1)) # if len(cp) <=3, degree must be less than that. but always >=1
+    crv.degree = max(1, min(degree, len(controlpts)-1)) # if len(cp) <=3, degree must be less than that. but always >=1
     # Set control points
     crv.ctrlpts = controlpts
     # Auto−generate the knot vector
@@ -113,11 +149,33 @@ def connected_points():
     line = ipv.plot([node1[0], node3[0]], [node1[1], node3[1]], [node1[2], node3[2]], color='blue')
     line = ipv.plot([node1[0], node4[0]], [node1[1], node4[1]], [node1[2], node4[2]], color='blue')
     ipv.show()
-
-
-    pass
-def sweep_surface():
     return
+
+def sweep_surface(cross_section_vectors, spline_vectors):
+    """
+    Sweeps the cross_section_vectors along the direction of the spline_vectors
+    Returns a list of cross sectional vectors rotated to follow the curvature of the spline
+
+    Inputs:
+        cross_section_vectors: List<List<float, float, float>>
+        spline_vectors: List<List<float, float, float>>
+    Outputs:
+        List<List<List<float, float, float>>>
+    """
+    layers = []
+    z_vector = np.array([0,0,1])
+    for i in range(len(spline_vectors)-1):
+        dir_vec = np.array([spline_vectors[i+1][0] - spline_vectors[i][0],
+                   spline_vectors[i+1][1] - spline_vectors[i][1], 
+                   spline_vectors[i+1][2] - spline_vectors[i][2] ])
+        angle_rad = angle_between(dir_vec, z_vector)
+        # print(f"shape x: {z_vector.reshape(1, -1)}")
+        # print(f"shape v: {dir_vec.reshape(-1, 1)}")
+        rotated_vectors = rotate_vectors(cross_section_vectors, np.cross([0,0,1], dir_vec), angle_rad, False)
+        translated_vectors = rotated_vectors + spline_vectors[i]
+        layers.append(translated_vectors)
+
+    return np.array(layers)
 
 
 
@@ -126,7 +184,7 @@ def test():
     print("hello world")
     return 2.5
 def example1_circle():
-    v = create_circle_vectors(50, 50, 0)
+    v = get_circle_vectors(50, 50, 0)
     v1 = v.copy()
     for i in range(len(v)):
         v1.append(rotate_vector(v[i], [0,1,0], 180))
@@ -196,8 +254,30 @@ def example4_animation():
     ipv.show()
     return
 def example5_controlpoints():
-    cps = [[10,5,10], [10,20,-30], [40,10,25], [-10,5,0]]
-    spline_vectors = get_spline_vectors_from_controlpts(cps, 40)
+    cps = [[10,5,-10], [10,20,-30], [40,10,25], [-10,5,0]]
+    spline_vectors = get_spline_vectors_from_controlpts(cps, 40,2)
     visualise_vectors(spline_vectors)
+def example6_rotatecirclevectors():
+    vectors = get_circle_vectors(5)
+    v_rotated = rotate_vectors(vectors, [1,0,0], 45)
+    vectors += v_rotated
+    visualise_vectors(vectors)
+def example7_sweep():
+    circle = get_circle_vectors(5,20, 0)
+    spline = get_spline_vectors_from_controlpts([[0,0,0], [1,1,1]], 10)
+    vectors = sweep_surface(circle, spline)
+    print(f"shape: {vectors.shape}")
+    visualise_vectors(vectors.reshape(-1, vectors.shape[-1]))
+    return
+def example8_sweep():
+    circle = get_circle_vectors(5,40, 0)
+    cps = [[10,5,-10], [10,20,-30], [40,10,25], [-10,5,0]]
+    spline = get_spline_vectors_from_controlpts(cps, 40,2)
+    vectors = sweep_surface(circle, spline)
+    visualise_vectors(vectors.reshape(-1, vectors.shape[-1]))
+    return
 # %%
-example5_controlpoints()
+# example6_rotatecirclevectors()
+example8_sweep()
+
+# %%
